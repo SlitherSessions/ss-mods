27a056bbab738b0e594d8410bf6850ef75de0e87
Axe Tota <axetota@gmail.com>
2016-05-27 09:43:05 -0400

    Drafted first version of SlitherSessions Mod

---
+++ js/social.js
@@ -0,0 +1,54 @@
+window.onerror = function (message, url, lineNumber) {
+  console.log(message+', '+url+', '+lineNumber);
+}
+
+var sdan = false; // social done animating
+var sshfr = 0;
+var social = document.getElementById('social');
+var ytfhol = document.getElementById('ytfhol');
+var fbfhol = document.getElementById('fbfhol');
+var twfhol = document.getElementById('twfhol');
+
+var cssoc = false;
+
+var load_tm = Date.now();
+var oef_iv = -1;
+
+var ltm = Date.now();
+
+function oef () {
+ var ctm = Date.now();
+ vfr = (ctm - ltm)/8;
+ ltm = ctm;
+ if (! cssoc) { // can show social
+  if (social.offsetHeight > 66) { // ok
+    cssoc = true;
+    load_tm = Date.now(); // a lie, but keeps the loop alive til the animation is done
+  } else if (ctm - load_tm > 6000) { // too long to load. just give up
+    clearInterval(oef_iv);
+  }
+ } else if (!sdan) { //social done animating
+  sshfr += vfr/203;
+  var k = Math.pow(Math.min(1, Math.max(0, sshfr)), .5);
+  if (ytfhol) {
+   ytfhol.style.opacity = .8*k;//Math.sin(2.3 * k);
+   //ytfhol.style.marginTop = (Math.round((9 - 9*k)*1000)/1000) + 'px';
+  }
+  var k = Math.pow(Math.min(1, Math.max(0, sshfr - .22)), .5);
+  if (fbfhol) {
+   fbfhol.style.opacity = .8*k;//Math.sin(2.3 * k);
+   //fbfhol.style.marginTop = (Math.round((15 - 9*k)*1000)/1000) + 'px';
+  }
+  var k = Math.pow(Math.min(1, Math.max(0, sshfr - .44)), .5);
+  if (twfhol) {
+   twfhol.style.opacity = .8*k;//Math.sin(2.3 * k);
+   //twfhol.style.marginTop = (Math.round((15 - 9*k)*1000)/1000) + 'px';
+  }
+  if (sshfr >= 1.44) {
+   sdan = true;
+   clearInterval(oef_iv);
+  }
+ }
+}
+
+oef_iv = setInterval('oef()', 25);
--- sync-s3.sh
+++ sync-s3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 find . -name '.DS_Store' -delete
-exec aws s3 sync ./ s3://slither.axetota.com --acl "public-read" \
+exec aws s3 sync ./ s3://slither.kushview.net --acl "public-read" \
   --delete --exclude ".DS_Store" --exclude "sync-s3.sh" \
   --exclude "*git*" --exclude ".gitignore"
