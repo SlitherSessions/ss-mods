0f773581855f459954081a5361af6796b9c9b4f6
Michael Fisher <mfisher@kushview.net>
2016-05-30 14:12:18 -0400

    Added GA code

--- sync-s3.sh
+++ sync-s3.sh
@@ -4,7 +4,8 @@ set -e
 ./build.sh
 
 find . -name '.DS_Store' -delete
-exec aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --delete \
+echo "=== Uploading MODS"
+aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --delete \
   --exclude ".DS_Store" \
   --exclude "sync-s3.sh" \
   --exclude "build.sh" \
@@ -16,7 +17,9 @@ exec aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --del
   --exclude "src*" \
   --exclude "chrome*" \
   --exclude "*.zip"
-exec aws s3 sync ./www s3://www.slithersessions.com  --acl "public-read" --delete \
+
+echo "=== Uploading WWW"
+aws s3 sync ./www s3://www.slithersessions.com  --acl "public-read" --delete \
   --exclude ".DS_Store" \
   --exclude "sync-s3.sh" \
   --exclude "build.sh" \
