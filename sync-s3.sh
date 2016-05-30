60388b67c7f1b746f2eeff32e17965972170c602
Axe Tota <axetota@gmail.com>
2016-05-30 10:35:45 -0400

    Split into mods subddir. add www placeholder for main site

--- sync-s3.sh
+++ sync-s3.sh
@@ -4,7 +4,19 @@ set -e
 ./build.sh
 
 find . -name '.DS_Store' -delete
-exec aws s3 sync ./ s3://mods.slithersessions.com  --acl "public-read" --delete \
+exec aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --delete \
+  --exclude ".DS_Store" \
+  --exclude "sync-s3.sh" \
+  --exclude "build.sh" \
+  --exclude "*git*" \
+  --exclude ".gitignore" \
+  --exclude "js/main.js" \
+  --exclude "js/social.js" \
+  --exclude "test.html" \
+  --exclude "src*" \
+  --exclude "chrome*" \
+  --exclude "*.zip"
+exec aws s3 sync ./www s3://www.slithersessions.com  --acl "public-read" --delete \
   --exclude ".DS_Store" \
   --exclude "sync-s3.sh" \
   --exclude "build.sh" \
