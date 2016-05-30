15f0a633d34ab6f20b06c39a5059885b450d7eba
Michael Fisher <mfisher@kushview.net>
2016-05-30 10:35:45 -0400

    Split into mods subddir. add www placeholder for main site

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -ex
-yuicompressor -o js/main.min.js src/main.js
-yuicompressor -o js/social.min.js src/social.js
-yuicompressor -o css/style.min.css css/style.css
+
+yuicompressor -o mods/js/main.min.js src/main.js
+yuicompressor -o mods/js/social.min.js src/social.js
+yuicompressor -o mods/css/style.min.css mods/css/style.css
 
 rm -f *.zip
 zip -r slither-sessions-chrome.zip chrome
