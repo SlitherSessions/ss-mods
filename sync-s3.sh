b90b5f4821a939ada37d0eda7dc3b67387eb4e0a
Axe Tota <axetota@gmail.com>
2016-05-31 08:47:02 -0400

    tidy up www sync to s3 (include JS

--- sync-s3.sh
+++ sync-s3.sh
@@ -4,7 +4,7 @@ set -e
 ./build.sh
 
 find . -name '.DS_Store' -delete
-echo "=== Uploading MODS"
+echo "=== Uploading MODS ==="
 aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --delete \
   --exclude ".DS_Store" \
   --exclude "sync-s3.sh" \
@@ -18,15 +18,14 @@ aws s3 sync ./mods s3://mods.slithersessions.com  --acl "public-read" --delete \
   --exclude "chrome*" \
   --exclude "*.zip"
 
-echo "=== Uploading WWW"
+echo
+echo "=== Uploading WWW ==="
 aws s3 sync ./www s3://www.slithersessions.com  --acl "public-read" --delete \
   --exclude ".DS_Store" \
   --exclude "sync-s3.sh" \
   --exclude "build.sh" \
   --exclude "*git*" \
   --exclude ".gitignore" \
-  --exclude "js/main.js" \
-  --exclude "js/social.js" \
   --exclude "test.html" \
   --exclude "src*" \
   --exclude "chrome*" \
