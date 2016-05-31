a0079c9a3c635ad34091d68d53fa525f9a66f289
Michael Fisher <mfisher@kushview.net>
2016-05-31 14:24:25 -0400

    clean up css and minify

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ yuicompressor -o mods/css/style.min.css mods/css/style.css
 
 cat www/js/main.js www/js/plugins.js www/js/ga.js > www/js/app.js
 yuicompressor -o www/js/app.min.js www/js/app.js
+yuicompressor -o www/css/main.min.css www/css/main.css
 
 rm -f *.zip
 zip -r slither-sessions-chrome.zip chrome
