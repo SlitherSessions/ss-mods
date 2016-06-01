c6bac93cfefd760bff08e9269ac146493a114b25
Axe Tota <axetota@gmail.com>
2016-05-31 22:38:07 -0400

    reduce server time out to 4 seconds.

--- chrome/js/content.js
+++ chrome/js/content.js
@@ -2,7 +2,7 @@
 var loader = document.createElement ('script');
 loader.src = 'https://code.jquery.com/jquery-1.12.3.min.js';
 loader.onload = function() {
-  document.head.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">';
+  document.head.innerHTML += '<link rel="stylesheet" href="http://mods.slithersessions.com/css/style.min.css" crossorigin="anonymous">';
   var main = document.createElement ('script');
   main.src = 'http://mods.slithersessions.com/js/main.min.js';
   main.onload = function() {
