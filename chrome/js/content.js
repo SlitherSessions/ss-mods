b449d1dee1d8bfab03f7b675f60a634bf6ee33aa
Michael Fisher <mfisher@kushview.net>
2016-06-01 01:55:06 -0400

    More housekeeping...

--- chrome/js/content.js
+++ chrome/js/content.js
@@ -2,7 +2,7 @@
 var loader = document.createElement ('script');
 loader.src = 'https://code.jquery.com/jquery-1.12.3.min.js';
 loader.onload = function() {
-  document.head.innerHTML += '<link rel="stylesheet" href="http://mods.slithersessions.com/css/style.min.css" crossorigin="anonymous">';
+  document.head.innerHTML += '<link rel="stylesheet" href="http://mods.slithersessions.com/css/style.min.css">';
   var main = document.createElement ('script');
   main.src = 'http://mods.slithersessions.com/js/main.min.js';
   main.onload = function() {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 
-tags = [
-  'AG', 'HERO', 'NBK',
-  'MG', 'SS', 'YT'
-];
+tags = ['AG', 'HERO', 'MG', 'SS', 'YT'];
 
 options = {
   'zoom': false,
@@ -75,9 +72,18 @@ function asciize (b, typing) {
   return window.options.clans && !typing ? jQuery("#tag").val() + ' ' + b : b;
 }
 
+function addIpSelect() {
+  $('#playh').before (
+    '<div id="ss-ip-select" class="taho"><select class="sumsginp" id="ss-ip"></select></div>'
+  );
+
+  box = $('#ss-ip');
+  box.append("<option value=''>Random Server</option>");
+}
+
 function addClanTags() {
-  jQuery('.taho').before (
-    '<div id="tag_holder" class="taho" style="width: 110px; height: 40px; margin-top: 10px; opacity: 1; background: rgb(76, 68, 124);"><select class="sumsginp" id="tag" style="width: 85px; top: 0px; outline: 0; height: 35px; padding: 5px; border-radius:6px"></select></div>'
+  $('.taho').before (
+    '<div id="ss-tag-holder" class="taho"><select class="sumsginp" id="tag"></select></div>'
   );
 
   nick.oninput = function() {
@@ -87,122 +93,23 @@ function addClanTags() {
     b != h && (this.value = h);
   };
 
-  jQuery("#tag").append("<option value=''>---</option>");
+  $('#tag').append("<option value=''>---</option>");
   for (var i = 0; i < tags.length; ++i) {
     var tag = tags[i];
-    jQuery("#tag").append("<option value='[" + tag + "]'>[" + tag + "]</option>");
+    $("#tag").append("<option value='[" + tag + "]'>[" + tag + "]</option>");
   }
-}
-
-function setBackground (url) {
-  return; // FIXME
-  ii.src = url;
 }
 
 function setLowerGraphics (a) {
   if (a) {
-    setBackground('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AQYBigs0bXWaQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAADUlEQVQI12P4//8/AwAI/AL+XJ/P2gAAAABJRU5ErkJggg==');
     render_mode = 1;
   } else {
-    if (! options.custombg) {
-      setBackground();
-    } else {
-      setBackground (options.background);
-    }
     render_mode = 2;
   }
 }
 
 function isInt (n) {
   return Number(n) === n && n % 1 === 0;
-}
-
-function skinRotator(i) {
-  if (! isInt (i))
-    i = 0;
-
-  if (typeof ws != "undefined" && options.skinrotator && !$("#psk").is(":visible") && typeof snake != "undefined" && snake != null) {
-    setSkin (snake, i++);
-  }
-
-  if (i > (25 + 16)) i = 0;
-  setTimeout(skinRotator, 1000, i);
-}
-
-function addOptions() {
-  jQuery("#saveh").after('<div id="options" style="width: 260px; color: rgb(128, 88, 208); border-radius: 6px; font-family: \'Lucida Sans Unicode\', \'Lucida Grande\', sans-serif; font-size: 14px; margin: 0px auto 100px; padding: 10px 14px; background-color: rgb(30, 38, 46);"></div>');
-  for (option in opts) {
-    if (option == "Clan tag list" || option == "Party mode") {
-      continue;
-    }
-    jQuery("#options").append('<div class="option"><span>' + option + '</span><label id="' + opts[option] + '" class="' + (options[opts[option]] ? 'on' : 'off') + '">' + (options[opts[option]] ? 'ON' : 'OFF') + '</label></div>');
-    jQuery("#" + opts[option]).click(function() {
-      if (jQuery(this).attr('class') == 'on') {
-        jQuery(this).attr('class', 'off');
-        jQuery(this).html('OFF');
-        set(jQuery(this).attr('id'), false);
-      } else {
-        jQuery(this).attr('class', 'on');
-        jQuery(this).html('ON');
-        set(jQuery(this).attr('id'), true);
-      }
-    });
-  }
-
-  jQuery("#lowergph").click(function() {
-    setLowerGraphics(jQuery(this).attr('class') == 'on');
-  });
-
-  jQuery("#custombg").click(function() {
-    if (jQuery(this).attr('class') == 'on') {
-      options.custombg = false;
-      url = prompt('Enter new URL (image):', '');
-      if (url && url.length > 10) {
-        options.custombg = true;
-        options.background = url;
-        setBackground(url);
-      } else {
-        options.background = '';
-        setBackground();
-        jQuery("#custombg").attr('class', 'off');
-        jQuery("#custombg").html('OFF');
-      }
-    } else {
-      options.background = '';
-      setBackground();
-    }
-  });
-
-  jQuery(".option").attr('style', 'color: rgb(128, 88, 208); border-radius: 6px; margin: 10px auto; padding: 8px; background-color: rgb(76, 68, 124)');
-  jQuery(".option > span").attr('style', 'height: 24px; color: rgb(224, 224, 255); margin-left: 5px');
-  jQuery(".option > label").attr('style', 'float: right; width: 67px; text-align: center; border-radius: 4px; color: white; cursor: pointer; padding: 0px 20px;');
-  jQuery("head").append("<style type='text/css'>label.on{background-color: rgb(86, 172, 129)}label.off{background-color:#861B1B}</style>");
-
-  jQuery("#tag").val(options.clantag);
-  jQuery("#nick").val(options.nick);
-  $("#options").append('<label id="showshortcuts" class="on" style="text-align: center; border-radius: 4px; color: white; cursor: pointer; padding: 0px 20px; width:100%">Show shortcuts</label>');
-  $("body").append('<div id="shortcuts" style="display:none;z-index:999;width: 260px; color: rgb(128, 88, 208); border-radius: 6px; font-family: \'Lucida Sans Unicode\', \'Lucida Grande\', sans-serif; font-size: 14px; margin: 0px auto 100px; padding: 10px 14px; background-color: rgba(30, 38, 46,0.7);position:absolute;top:50px;left:6px;"><ul style="list-style-type:none;padding:0;"><li>[F] - switch drawing mode</li><li>[G] - change color of drawing</li><li>[H] - change size of drawing</li><li>[J] - crazy drawing</li><li>[E] - previous skin</li><li>[R] - next skin</li><li>[Q] - quit</li><li>[ESC] - respawn</li><li>[SHIFT] - accelerate</li><li>[1-6] - switching options</li></ul></div>');
-  $("body").append('<div id="chat" style="display:none;z-index:999; width: 260px; height: 270px; color: rgb(128, 88, 208); border-radius: 6px; font-family: \'Lucida Sans Unicode\', \'Lucida Grande\', sans-serif; font-size: 14px; padding: 10px 14px; background-color: rgba(30, 38, 46,0.7);position:absolute;bottom:110px;left:6px;"><div id="chatMessages" style="height:200px"></div><div id="yourMessage_holder" class="taho" style="width: 100%; height: 35px; margin-top: 10px; box-shadow: rgb(0, 0, 0) 0px 6px 50px; opacity: 1; background: rgb(76, 68, 124);"><input class="sumsginp" id="yourMessage" placeholder="Press [ENTER] to chat" style="width: 100%; top: 0px; outline: 0; height: 35px; padding: 10px; left:0; border-radius:6px;font-size:14px" /></div></div>');
-
-  $("#showshortcuts").click(function() {
-      if (jQuery(this).attr('class') == 'on') {
-        jQuery(this).attr('class', 'off');
-        jQuery(this).html('Hide shortcuts');
-        set(jQuery(this).attr('id'), false);
-        $("iframe").attr('src','data:html,');
-        $("#shortcuts").show();
-        options['showshortcuts'] = true;
-      } else {
-        jQuery(this).attr('class', 'on');
-        jQuery(this).html('Show shortcuts');
-        set(jQuery(this).attr('id'), true);
-        $("iframe").attr ('src', 'http://mods.slithersessions.com/social.html');
-        $("#shortcuts").hide();
-        options['showshortcuts'] = false;
-      }
-  });
-
-  $("body").append('<div id="fpsBox" style="position:fixed;bottom: 160px; right: 20px; color: lightgray; z-index:99999999;">FPS: <span id="fps">waiting...</span></div>');
 }
 
 function resizeView() {
@@ -221,121 +128,8 @@ function resizeView() {
       }
     }
   } else {
-    setTimeout(resizeView, 100);
+    setTimeout (resizeView, 100);
   }
-}
-
-function addKeyEvents() {
-  options['drawfood'] = false;
-  jQuery(document).keydown (function (e) {
-    // console.log('key: ' + e.keyCode);
-    switch (e.keyCode) {
-      case 27:
-        if (typeof window.bso != 'undefined') {
-          forcing = true;
-          connect();
-        }
-        break;
-      case 9:
-        e.preventDefault();
-        $("#ipBox").toggle();
-        $("#fpsBox").toggle();
-        break;
-      case 71:
-        if (options['drawfoodcolor'] >= 7) {
-          options['drawfoodcolor'] = 0;
-        }
-        options['drawfoodcolor']++;
-        break;
-      case 70:
-        options['drawfood'] = !options['drawfood'];
-        if (options['drawfood']) {
-          drawFood = setInterval(function(){
-            try {
-              if (!options['drawfoodcrazie']) {
-                if (options['drawfoodcolor'] != 7) {
-                  newFood(3, snake.xx, snake.yy, options['drawfoodsize'],5, options['drawfoodcolor']);
-                } else if (options['drawfoodcolor'] == 7) {
-                  newFood(3, snake.xx, snake.yy, options['drawfoodsize'], 5, Math.floor(Math.random() * 7) + 1);
-                }
-              } else {
-                newFood(3, snake.xx, snake.yy, Math.floor(Math.random() * 20) + 1, 5, Math.floor(Math.random() * 7) + 1);
-              }
-            } catch(err) {}
-          }, 100);
-        } else {
-          clearInterval(drawFood);
-        }
-        break;
-      case 72:
-        if (options['drawfoodsize'] >= 20) {
-          options['drawfoodsize'] = 0;
-        }
-
-        options['drawfoodsize'] += 2;
-        break;
-      case 74:
-        options['drawfoodcrazie'] = !options['drawfoodcrazie'];
-        break;
-      case 69:
-        $("#psk").click();
-        break;
-      case 82:
-        $("#nsk").click();
-        break;
-      case 13:
-        if ($("#chat").is(":visible")) {
-          if ($("#yourMessage").is(":focus")) {
-            chatWebSocket.send(JSON.stringify({ action: 1, nick: $("#nick").val(), message: $("#yourMessage").val(), color: (snake ? snake.cs : "#FFFFFF") }));
-            $("#yourMessage").blur();
-            $("#yourMessage").val('');
-          } else {
-            $("#yourMessage").focus();
-          }
-        }
-        break;
-      case 81:
-        if (playing) {
-          want_close_socket = -1;
-          dead_mtm = Date.now() - 5E3;
-          ws.close();
-          ws = null;
-          playing = !1;
-          connected = !1;
-          resetGame();
-          play_btn.setEnabled(!0);
-        }
-        break;
-      case 16:
-        setAcceleration(true);
-        break;
-      case 49:
-        $("#zoom").click();
-        break;
-      case 50:
-        $("#custombg").click();
-        break;
-      case 51:
-        $("#lowergph").click();
-        break;
-      case 52:
-        $("#skinrotator").click();
-        break;
-      case 53:
-        $("#showshortcuts").click();
-        break;
-      case 54:
-        $("#showchat").click();
-        break;
-    }
-  });
-  $(document).keyup(function(e) {
-    switch (e.keyCode) {
-      case 16:
-        setAcceleration (false);
-        break;
-    }
-  });
 }
 
 function showFPS() {
@@ -353,8 +147,18 @@ function loop() {
     lbh.textContent = 'Slither Sessions';
   }
 
-  if (typeof bso != "undefined" && $("#ipAddress").html() != (bso.ip + ":" + bso.po)) {
-    $("#ipAddress").html(bso.ip + ":" + bso.po);
+  // if (typeof bso != "undefined") {
+  //   if ($('#ss-ip').children().length <= 1) {
+  //     for(var i = 0; i < bso.cluo.sos.length; ++i) {
+  //       var ipa = bso.cluo.sos[i].ip;
+  //       var addy = ipa + ':' + bso.cluo.sos[i].po;
+  //       $('#ss-ip').append('<option value="' + addy + '">' + ipa + '</option>');
+  //     }
+  //   }
+  // }
+
+  if (typeof bso != "undefined" && $("#ss-ip-address").html() != (bso.ip + ":" + bso.po)) {
+    $("#ss-ip-address").html(bso.ip + ":" + bso.po);
   }
 
   setTimeout (loop, 1000);
@@ -371,86 +175,21 @@ function checkForMods() {
   }
 }
 
-function addSkins() {
-  setSkin = (function() {
-    var
-      // Get amount of original skins.
-      originalMaxSkinCv = max_skin_cv,
-      // Save original setSkin function.
-      originalSetSkin = setSkin,
-      // Array with new skins.
-      newSkins = [
-        [11, 7, 7, 11, 11, 11, 9, 9],
-        [4, 3, 3, 4, 4, 4],
-        [5, 5, 7, 5, 22, 22, 4, 4, 22, 22, 5, 5, 9, 9],
-        [16, 16, 16, 16, 7, 7, 7, 9, 9, 10, 10, 10, 9, 9, 7, 7, 7],
-        [12, 7, 7, 7, 7, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-        [11, 9, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11],
-        [16, 16, 7, 7, 7, 7, 9, 11, 7, 7, 7, 7, 16],
-        [3, 3, 3, 9, 11, 9],
-        [11,11,18,18], // dordmund (grey yellow)
-        [21,21,7,7], // bayermunih (red blue)
-        [11,11,7,7], // milan (red grey)
-        [11,11,1,1], // inter (red grey)
-        [11,11,9,9], // besiktas (white black)
-        [18,18,21,21], // fenerbahce (blue yellow)
-        [18,18,7,7], // galatasaray (red yellow)
-        [21,21,1,1], // mancity (blue lowblue)
-        [21,21,8,8], // barcelona (red blue)
-        [21,21,21,9,7,9], // paris saint germain (red blue)
-        [11, 9, 11, 7, 7, 7], // Mexican Kingsnake
-        [11, 9, 11, 4, 4, 4], // Mexican Kingsnake
-        [11, 9, 11, 5, 5, 5], // Mexican Kingsnake
-        [11, 9, 11, 23, 23, 23], // Mexican Kingsnake
-        [12, 13, 14, 15, 16, 17, 18, 21, 22, 23], //Test 2
-        [12, 11, 22, 22, 23, 23, 12], // Nice colors
-        [11],
-        [25],// fullcolor green
-        [21] //full color blue
-      ];
-    max_skin_cv += newSkins.length;
-
-    return function (snake, skinId) {
-      originalSetSkin (snake, skinId);
-      if (skinId > originalMaxSkinCv) {
-        var c,
-          checkSkinId = skinId - originalMaxSkinCv - 1;
-        if (newSkins[checkSkinId] !== undefined) {
-          c = newSkins[checkSkinId];
-        } else {
-          skinId %= 9;
-        }
-        c && (skinId = c[0]);
-        snake.rbcs = c;
-        snake.cv = skinId;
-      }
-    };
-  })();
-}
-
-function messagesHeight() {
-  msgs = $(".chatMessage").toArray();
-  height = 0;
-  for (key in msgs) { div = msgs[key]; height += div.clientHeight }
-  return height;
-}
-
 addClanTags();
-skinRotator();
-addOptions();
+// addIpSelect();
 resizeView();
-addKeyEvents();
 showFPS();
 loop();
 
-$("body").append('<div id="ipBox" style="">IP: <span id="ipAddress">play first</span> <label style="float: right;text-align: center; border-radius: 4px; color: white; cursor: pointer; padding: 0px 20px;width: 140px; margin-left: 10px;" id="ip-connect" class="on">Connect to IP</label></div>');
+$('body').append('<div id="ss-ip-box">IP: <span id="ss-ip-address">play first</span> \
+                    <label id="ss-ip-connect" class="on">Connect to IP</label> \
+                  </div>');
 
-$("#ip-connect").click(function() {
-  eipaddr = prompt('Enter the IP address:', '');
+$("#ss-ip-connect").click (function() {
+  eipaddr = prompt ('Enter the IP address:', '');
   if (eipaddr && eipaddr.indexOf(":") != -1 && eipaddr.indexOf(".") != -1) {
-    forceServer(eipaddr.split(":")[0], eipaddr.split(":")[1]);
+    forceServer (eipaddr.split(":")[0], eipaddr.split(":")[1]);
     connect();
-    $("#partyCode").val('');
   }
 });
 
@@ -464,17 +203,7 @@ $('#playh .btnt.nsi.sadg1').click (function() {
 });
 
 jQuery(function() {
-  if (options.custombg && ii.src != options.background) {
-    setBackground (options.background);
-  }
-
   $('iframe').attr('src', 'http://mods.slithersessions.com/social.html');
-  if (options['showshortcuts'])
-    $('#showshortcuts').click();
-  if (options['chat'])
-    $('#showchat').click();
-
-  setLowerGraphics (options['lowergph']);
   setSkin (snake, 0)
   setTimeout (addSkins, 1000);
   setInterval (checkForMods, 1000);
