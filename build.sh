66f16b58e0f9705393cd8a1f05d09fd48a61dfad
Axe Tota <axetota@gmail.com>
2016-07-09 15:11:36 -0400

    Make sure css dir exists in chrome folder

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ yuicompressor -o build/ss.min.js build/ss.js
 cat build/ss.min.js > mods/js/ss.min.js
 cat build/ss.min.js > chrome/js/ss.min.js
 
+mkdir -p chrome/css # in case the dir doesn't exist.
 yuicompressor -o mods/css/style.min.css mods/css/style.css
 cat mods/css/style.min.css > chrome/css/style.min.css
 
