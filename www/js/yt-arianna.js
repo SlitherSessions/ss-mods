0ba8b8aa1c5bb6c2d52477a1ebff8221903cb53e
Axe Tota <axetota@gmail.com>
2016-07-16 19:55:31 -0400

    Added arianna's stream overlay page

---
+++ www/js/yt-arianna.js
@@ -0,0 +1,9 @@
+var channel = {
+  channelId: 'UCeMmObDtel6wYhgV-1frSnA', // arianna
+  apiKey: 'AIzaSyBbSep6hU3F13KGqh9y81bUiOU9Mb01NAU',
+  apiUrl: 'https://www.googleapis.com/youtube/v3',
+
+  subCount: 0,
+  subGoal: 50,
+  refreshMillis: 5 * 1000
+};
---
+++ www/js/yt-slithersessions.js
@@ -0,0 +1,9 @@
+var channel = {
+  channelId: 'UCsIpq4M5xE3NC09m-rtJcqg', // slither sessions
+  apiKey: 'AIzaSyBbSep6hU3F13KGqh9y81bUiOU9Mb01NAU',
+  apiUrl: 'https://www.googleapis.com/youtube/v3',
+
+  subCount: 0,
+  subGoal: 200,
+  refreshMillis: 5 * 1000
+};
