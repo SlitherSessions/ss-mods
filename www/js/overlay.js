b3e26a3d68086cee72fc12e4d04daacff7a75f84
Michael Fisher <mfisher@kushview.net>
2016-07-16 19:55:31 -0400

    Added arianna's stream overlay page

--- www/js/overlay.js
+++ www/js/overlay.js
@@ -1,13 +1,3 @@
-var channel = {
-  channelId: 'UCsIpq4M5xE3NC09m-rtJcqg',
-  apiKey: 'AIzaSyBbSep6hU3F13KGqh9y81bUiOU9Mb01NAU',
-  apiUrl: 'https://www.googleapis.com/youtube/v3',
-
-  subCount: 0,
-  subGoal: 175,
-  refreshMillis: 5 * 1000
-};
-
 function ssUpdateSubCount() {
   var endpoint = channel.apiUrl + '/channels';
 
