aa70c74e375b8e58e286f841fb2c701bc2cf7f5f
Axe Tota <axetota@gmail.com>
2016-07-23 14:16:05 -0400

    Version bump 2.1.14

--- mods/js/slithersessions.user.js
+++ mods/js/slithersessions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither Sessions
 // @namespace    mods.slithersessions.com
-// @version      2.1.13
+// @version      2.1.14
 // @description  Slither Sessions Mod Pack
 // @author       Kushview, LLC
 // @require      http://code.jquery.com/jquery-latest.js
