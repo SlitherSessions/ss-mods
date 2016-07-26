93b3d943be253a649a762fc6a5896ce85552a029
Michael Fisher <mfisher@kushview.net>
2016-07-26 03:59:33 -0400

    Remove www site form this repository

--- www/js/yt-slithersessions.js
+++
@@ -1,9 +0,0 @@
-var channel = {
-  channelId: 'UCsIpq4M5xE3NC09m-rtJcqg', // slither sessions
-  apiKey: 'AIzaSyBbSep6hU3F13KGqh9y81bUiOU9Mb01NAU',
-  apiUrl: 'https://www.googleapis.com/youtube/v3',
-
-  subCount: 0,
-  subGoal: 300,
-  refreshMillis: 5 * 1000
-};
