ee27fa69c9c755e3da7980b2be624c20da698c5c
Michael Fisher <mfisher@kushview.net>
2016-07-27 05:12:48 -0400

    Version bump

--- mods/js/slithersessions.user.js
+++ mods/js/slithersessions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither Sessions
 // @namespace    mods.slithersessions.com
-// @version      2.1.14
+// @version      2.1.15
 // @description  Slither Sessions Mod Pack
 // @author       Kushview, LLC
 // @require      http://code.jquery.com/jquery-latest.js
