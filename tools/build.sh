01a6be424ff25158d2dc613ca7663ebbea57816b
Axe Tota <axetota@gmail.com>
2016-08-30 21:39:06 -0400

    Housekeeping directory structure. Rename bot.js to gozer.js. added resources

--- tools/build.sh
+++ tools/build.sh
@@ -23,11 +23,19 @@
 
 set -ex
 
-# Splice out the bot code we care about
-awk 'NR >= 0 && NR < 2115' vendor/bot-ce/bot.user.js > src/bot.js
+mkdir -p build
 
-cat src/config.js src/bot.js src/ss.js src/skins.js src/clans.js \
+# Splice out the bot code we care about
+awk 'NR >= 0 && NR < 2115' vendor/bot-ce/bot.user.js > src/gozer.js
+
+cat src/config.js \
+  src/gozer.js \
+  src/ss.js \
+  src/resources.js \
+  src/skins.js \
+  src/clans.js \
   src/main.js > build/ss.js
+
 yuicompressor -o build/ss.min.js build/ss.js
 cat build/ss.js > mods/js/ss.js
 cat build/ss.min.js > mods/js/ss.min.js
@@ -38,6 +46,6 @@ mkdir -p chrome/css # in case the dir doesn't exist.
 yuicompressor -o mods/css/style.min.css mods/css/style.css
 cat mods/css/style.min.css > chrome/css/style.min.css
 
-# Make the chrome extension upload package
+# Make the chrome/opera extension upload package
 rm -f *.zip
 zip -rvT slither-sessions-chrome.zip chrome
