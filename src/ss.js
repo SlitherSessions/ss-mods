0b681fa34c154b75857421857b57356f9c6fb48d
Axe Tota <axetota@gmail.com>
2016-10-24 11:30:52 -0400

    version bump 2.2.7

--- src/ss.js
+++ src/ss.js
@@ -32,7 +32,7 @@ var ss = window.ss = (function() {
       useLastHost: false
     },
 
-    version: function() { return '2.2.6'; },
+    version: function() { return '2.2.7'; },
 
     isInt: function (n) {
       return !isNaN(n) && Number(n) === n && n % 1 === 0;
