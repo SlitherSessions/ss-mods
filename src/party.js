27da81ee763b9d38f3e8a3cf842b8dbc866674cb
Axe Tota <axetota@gmail.com>
2016-11-12 22:12:30 -0500

    Started party module

---
+++ src/party.js
@@ -0,0 +1,67 @@
+/*
+  party.js - This file is part of Slither Sessions Mods
+
+  The MIT License (MIT)
+  Copyright (c) 2016 Slither Sessions.  All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+  of the Software, and to permit persons to whom the Software is furnished to do
+  so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+ss.register ((function() {
+
+  var impl = {
+    currentParty: { },
+    apiKey: null,
+    endpoint: 'http://slithersessions.dev/v1',
+
+    url: function (action) {
+      return impl.endpoint + '/' + action
+    },
+
+    request: function (action, data, success, error) {
+      $.ajax ({
+        async: false,
+        type: "POST",
+        url: impl.url(action),
+        data: data,
+        success: success,
+        error: error,
+        dataType: 'JSONP'
+      });
+
+      return impl.currentParty;
+    },
+
+    created: function (response) {
+      console.log (response);
+    },
+  };
+
+  var party = {
+    slug: 'party',
+    init: function() {
+
+    },
+    create: function() {
+      impl.request ('parties/create.json', null, impl.created);
+    }
+  };
+
+  return party;
+})());
