3f15eff74bc3dade6a6693a7de1fda34cf020177
Michael Fisher <mfisher@kushview.net>
2017-05-15 12:59:04 -0400

    Version bump 2.2.18

--- src/ss.js
+++ src/ss.js
@@ -32,7 +32,7 @@ var ss = window.ss = (function() {
       useLastHost: false
     },
 
-    version: function() { return '2.2.17'; },
+    version: function() { return '2.2.18'; },
 
     isInt: function (n) {
       return !isNaN(n) && Number(n) === n && n % 1 === 0;
