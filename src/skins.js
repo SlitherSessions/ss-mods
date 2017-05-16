12fe1f0ad483c726a36b7f139df4eb004ee49c4a
Axe Tota <axetota@gmail.com>
2017-05-16 17:18:58 -0400

    First draft KKA skin

--- src/skins.js
+++ src/skins.js
@@ -271,24 +271,24 @@ ss.register ((function() {
                       x: -100,
                       y: -80
                     }
+                  })
+            .add ({ rbcs: [ 34,34,34,34, 11,11,11,11 ],
+                    stockSkinId: 8,                              // Kitkat Angel
+                    antenna: {
+                      alpha: 0.5,
+                      color2: "#f740ff",
+                      color1: "#f740ff"
+                    },
+                    bulb: {
+                      image: ss.resources.images.kitkatAngel,
+                      scale: 0.70,
+                      alpha: 1.0,
+                      x: -46,
+                      y: -56,
+                      width: 100,
+                      height: 100
+                    }
                   });
-            // .add ({ rbcs: [ 1, 1, 1, 2, 2, 2 ],
-            //         stockSkinId: 4,                              // Kitkat Angel
-            //         antenna: {
-            //           alpha: 0.5,
-            //           color2: "#c78b31",
-            //           color1: "#966308"
-            //         },
-            //         bulb: {
-            //           image: ss.resources.images.teddyBear,
-            //           scale: 0.22,
-            //           alpha: 1.0,
-            //           x: -100,
-            //           y: -80,
-            //           width: 100,
-            //           height: 100
-            //         }
-            //       });
 
       window.setSkin = function (snk, skinId) {
         skinId = parseInt (skinId);
